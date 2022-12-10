8758fd683e8dfd213f0aafa6e244b6db50bdcb12
Jacqui Li <jil659@g.harvard.edu>
2022-12-10 05:02:26 +0000

    added some simple styles

--- p3/views/templates/master.blade.php
+++ p3/views/templates/master.blade.php
@@ -9,7 +9,7 @@
 
     <link rel='shortcut icon' href='/favicon.ico'>
 
-    <link href='/css/app.css' rel='stylesheet'>
+    <link rel="stylesheet" href="/css/p3.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
@@ -17,7 +17,7 @@
 
 </head>
 
-<body id="body-origin">
+<body id="body">
 
     <header>
         <h1>{{ $app->config('app.name') }}</h1>
