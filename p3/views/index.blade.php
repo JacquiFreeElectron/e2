45431e5cd8e63d675de2841b0ddc55310ac8ffae
Jacqui Li <jil659@g.harvard.edu>
2022-12-10 04:50:17 +0000

    checked user's previous choice

--- p3/views/index.blade.php
+++ p3/views/index.blade.php
@@ -8,11 +8,12 @@
     <h2>Let's Play Rock, Paper, and Scissors!</h2>
     <p>You are going to play with your computer. Please choose your move : </p>
     <form method='POST' action="/process">
-        <input type='radio' id='rock' name='choice' value='rock'>
+        <input type='radio' id='rock' name='choice' value='rock' @if ($choice == 'rock') checked @endif>
         <label for='rock'><img src='/images/rock-svgrepo-com.svg' height=25px width=25px> rock </label>
-        <input type='radio' id='paper' name='choice' value='paper'>
+        <input type='radio' id='paper' name='choice' value='paper' @if ($choice == 'paper') checked @endif>
         <label for='paper'><img src='/images/paper-svgrepo-com.svg' height=25px width=25px> paper </label>
-        <input type='radio' id='scissors' name='choice' value='scissors'>
+        <input type='radio' id='scissors' name='choice' value='scissors'
+            @if ($choice == 'scissors') checked @endif>
         <label for='scissors'><img src='/images/scissors-svgrepo-com.svg' height=25px width=25px> scissors</label>
 
         <button type='submit'>submit</button>
